15f9a1bb62972a9123a3d4bfb16e7bd79c66d47c
hurrytospring <ddyz1993@163.com>
2019-07-16 17:28:35 +0800

    cg to azure

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -22,29 +22,29 @@ module.exports = {
     // "production" is the environment name
     production: {
       // SSH key path, default to $HOME/.ssh
-      key: "~/Desktop/relay",
+    //   key: "~/Desktop/relay",
       // SSH user
-      user: "ubuntu",
+      user: 'ubuntu',
       // SSH host
-      host: ["124.156.121.100"],
+      host: ['52.229.172.41'],
       // SSH options with no command-line flag, see 'man ssh'
       // can be either a single string or an array of strings
-      ssh_options: "StrictHostKeyChecking=no",
+      ssh_options: 'StrictHostKeyChecking=no',
       // GIT remote/branch
-      ref: "origin/vite",
+      ref: 'origin/vite',
       // GIT remote
-      repo: "https://github.com/vitelabs/node-walletconnect-bridge.git",
+      repo: 'https://github.com/vitelabs/node-walletconnect-bridge.git',
       // path in the server
-      path: "/var/www/bifrost",
+      path: '/var/www/bifrost',
       // Pre-setup command or path to a script on your local machine
       //   // Post-setup commands or path to a script on the host machine
       //   // eg: placing configurations in the shared dir etc
-      "post-setup": "ls -la",
+      'post-setup': 'ls -la',
       // pre-deploy action
       //   pre-deploy-local: "echo 'This is a local executed command'"
       // post-deploy action
-      "post-deploy":
-        "npm install && npm run build && pm2 start ecosystem.config.js"
+      'post-deploy':
+        'npm install && npm run build && pm2 start ecosystem.config.js'
     }
   }
 };
